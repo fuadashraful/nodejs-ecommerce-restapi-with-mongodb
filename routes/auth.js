3fa69f0c10a6d01a90c5963d510cbf0536470b30
fuadashraful <fuadashraful16@gmail.com>
2021-11-12 15:02:16 +0600

    user crud api implemented

--- routes/auth.js
+++ routes/auth.js
@@ -9,7 +9,7 @@ route.post('/register',async (req,res)=>{
     const newUser=new User({
         username:req.body.username,
         email:req.body.email,
-        password:CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SECRET),
+        password:CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SECRET).toString(),
     });
 
     try{
